bbc5c6346b465601ffcb66d9458acf521c708815
Oscar Herrera <osedhelu@gmail.com>
2023-11-17 00:37:30 -0500

    update(img): implement text

--- .vercel/output/functions/render.func/chunks/generic_443d1125.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_ae356942.mjs').then(n => n.a);
-
-export { page };
