4d1054a841acaf938b9db04d4c8148d9c9cd04fe
Oscar Herrera <osedhelu@gmail.com>
2023-11-17 00:11:46 -0500

    add(service-notification): implementar service

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,34 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			textShadow: {
+				'default': '0 2px 5px rgba(0, 0, 0, 0.5)',
+				'lg': '0 2px 10px rgba(0, 0, 0, 0.5)',
+			},
+			colors: {
+				primary: {
+					50: '#f2f6ee',
+					100: '#e1ead7',
+					200: '#ced7b2',
+					300: '#c1c186',
+					400: '#af9f64',
+					500: '#9d7d53',
+					600: '#875b45',
+					700: '#6e3e3a',
+					800: '#5e363b',
+					900: '#52323e',
+					950: '#2e1a25',
+				},
+			}
+		},
 		fontFamily: {
 			sans: ['Gotham SSm A, sans-serif']
+		}
+	},
+	variants: {
+		extend: {
+			textShadow: ['responsive', 'hover', 'focus'],
 		}
 	},
 	plugins: [],
