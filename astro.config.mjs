42c696aa890f2299051c09dc3abc720c49d3ea93
Oscar Herrera <osedhelu@gmail.com>
2023-12-06 13:18:11 -0500

    init(admin.js): update docker-compose db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
-import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
-import { defineConfig } from 'astro/config'
-
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  integrations: [tailwind(), react()]
-
-})
+export default defineConfig({});
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,2 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-declare module 'mdx-mermaid/Mermaid'
-declare global {
-    interface Window { callback: any; }
-
-}
-// env.d.ts
-declare module 'react-map-interaction' {
-    export const MapInteractionCSS: React.ComponentType<any>;
-
-}
