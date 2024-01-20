c69d694d95058d07e124f9fc293c8414a27b9176
Oscar Herrera <osedhelu@gmail.com>
2024-01-19 22:26:47 -0500

    feat(hardhat): add hardhat smart contract

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -22,7 +22,19 @@ export default {
     extend: {
       colors: {
         primary: '#0a0a0a',
-        accent: '#B809C3'
+        accent: '#B809C3',
+        secondary: '#6448a5',
+        "blue-50": "#F5F3FF",
+        "blue-100": "#EDE9FE",
+        "blue-200": "#DDD6FE",
+        "blue-300": "#C4B5FD",
+        "blue-400": "#A78BFA",
+        "blue-500": "#8B5CF6",
+        "blue-600": "#7C3AED",
+        "blue-700": "#6D28D9",
+        "blue-800": "#5B22D0",
+        "blue-900": "#4C1DAB",
+
       },
       backgroundImage: {
         site: "url('/assets/site-bg.svg')",
